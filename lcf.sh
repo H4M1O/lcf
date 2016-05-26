b20b2ca667d92153a2c8dfdf1a8f63b1294f520e
Claudio Proietti <claudio.proietti@hackmind.org>
2016-05-26 21:04:53 +0100

    Added new functionalities. V.2.0.0

--- lcf.sh
+++ lcf.sh
@@ -1,15 +1,16 @@
 #!/bin/bash                                                                                                                                     
 # Script: Linux Configuration File                                             
 # Description: LCF is a simple script to personalize your Linux distro as H4M1O.
-# Version: 1.0.0                                                                 
-# Date: 20-05-2016                                                               
+# Version: 2.0.0                                                                 
+# Date: 26-05-2016                                                               
 # Author: Claudio Proietti                                                       
 # License: The MIT License (MIT) - Copyright (c) 2016 Claudio Proietti
 
-function main ()                                                                                                                                     
-{                                                                                    
+function main ()                                                                                                                                 {                                                                                    
     # This is the main function that show the menu and allows the user to make a choice
     # declared integer for user's choice                                             
+    sudo apt-get update 
+    sudo apt-get upgrade -y
     clear                                                                            
     declare -i OPT                                                                   
     while true                                                                       
@@ -17,14 +18,27 @@ function main ()
         menu                                                                         
         # read input from keyboard                                                   
         read OPT                                                                     
-        if [ $OPT -ge 0 -a $OPT -le 2 ]                                              
+        if [ $OPT -ge 0 -a $OPT -le 6 ]                                              
         then                                                                         
             case $OPT in                                                             
-                1 ) install                                                     
-                echo -e "$(tput setaf 0)$(tput setab 2)\nAPPS INSTALLATION COMPLETED!$(tput sgr 0)\n"
+                1 ) bash_cfg                                                     
+                echo -e "$(tput setaf 0)$(tput setab 2)\nBASH CONFIGURATION COMPLETED!$(tput sgr 0)\n"
                 ;;                                                                   
-                2 ) configure
-                echo -e "$(tput setaf 0)$(tput setab 2)\nAPPS CONFIGURATION COMPLETED!$(tput sgr 0)\n"                
+                2 ) vim_cfg
+                echo -e "$(tput setaf 0)$(tput setab 2)\nVIM INSTALLATION AND CONFIGURATION COMPLETED!$(tput sgr 0)\n"                
+                ;;
+                3 ) tmux_cfg                                                     
+                echo -e "$(tput setaf 0)$(tput setab 2)\nTMUX INSTALLATION AND CONFIGURATION COMPLETED!$(tput sgr 0)\n"
+                ;;                                                                   
+                4 ) i3_cfg
+                echo -e "$(tput setaf 0)$(tput setab 2)\nI3 INSTALLATION AND CONFIGURATION COMPLETED!$(tput sgr 0)\n"                
+                ;;
+                5 ) sec_cfg                                                     
+                echo -e "$(tput setaf 0)$(tput setab 2)\nSECURITY INSTALLATION AND CONFIGURATION COMPLETED!$(tput sgr 0)\n"
+                ;;                                                                   
+                6 ) all_cfg
+                echo -e "$(tput setaf 0)$(tput setab 2)\nAPPS INSTALLATION AND CONFIGURATION COMPLETED!$(tput sgr 0)\n"
+		exit 1                
                 ;;
                 0 ) clear                                                            
                 echo -e "$(tput setaf 7)$(tput setab 0)\nThank you for using this script!$(tput sgr 0)\n"
@@ -39,33 +53,62 @@ function main ()
     done                                                                             
 }                              
 
-function menu ()                                                                                                                                {                                                                                
+function menu ()
+{ 
 # The scope of this function is only to show the contextual menu                 
     echo "$(tput setaf 3)$(tput bold)Welcome to L.C.F. - Linux Configuration Files"
     echo -e "Script created by Claudio Proietti under MIT license$(tput sgr 0)\n"
     echo -e "These are the available options:\n"                                 
-    echo "1 - INSTALL APPLICATION (Only with ROOT and install VIM, I3 and TMUX)"       
-    echo "2 - CONFIGURE APPS (With current user and it can be used with root)" 
+    echo "1 - CONFIGURE BASH"       
+    echo "2 - INSTALL AND CONFIGURE VIM" 
+    echo "3 - INSTALL AND CONFIGURE TMUX"       
+    echo "4 - INSTALL AND CONFIGURE I3" 
+    echo "5 - INSTALL AND CONFIGURE SSH" 
+    echo "6 - INSTALL AND CONFIGURE ALL THE APPS" 
     echo -e "\n0 - Exit the script\n"                                            
     echo "$(tput setaf 3)Write now the option that you want select and press enter: $(tput sgr 0)"  
 }      
 
-function install ()
+function bash_cfg ()
 {
-    apt-get update
-    apt-get upgrade -y
-    apt-get install vim i3 tmux -y
+    cp .bashrc ~/
+    source ~/.bashrc
 }
 
-function configure ()
+function vim_cfg ()
 {
-    mkdir ~/.vim/bundle ~/.vim/colors ~/.vim/.backup ~/.vim/.undo ~/.vim/.swap
-    mkdir ~/.i3
-    cp -fv molokai/colors/molokai.vim ~/.vim/colors
-    cp -fv .vimrc ~/
-    cp -fv .plugins_vim ~/
-    cp -fv .i3/config ~/.i3/
-    cp -R neobundle.vim ~.vim/
+    sudo apt-get install vim
+    cp -r .vim ~/
+    cp .vimrc ~/
+    cp .plugins_vim ~/
+}
+
+function tmux_cfg ()
+{
+    sudo apt-get install tmux
+    cp .tmux.conf ~/
+}
+
+function i3_cfg ()
+{
+    sudo apt-get install i3
+    cp -r .i3 ~/
+    cp etc/i3status.conf /etc/i3status.conf
+}
+
+function sec_cfg ()
+{
+    sudo apt-get install openssl-client
+    ssh-keygen -b 4096 -t rsa
+}
+
+function all_cfg ()
+{
+    bash_cfg
+    vim_cfg
+    tmux_cfg
+    i3_cfg
+    sec_cfg
 }
 
 #Call to the main function
