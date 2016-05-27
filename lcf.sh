8a303298ec3f9f0ba50d45cadb56cdd724e3e5f6
Claudio Proietti <claudio.proietti@hackmind.org>
2016-05-27 17:10:56 +0100

    Added 2 functions, the check of the OS and the sudo. V.2.2.2

--- lcf.sh
+++ lcf.sh
@@ -1,16 +1,38 @@
 #!/bin/bash                                                                                                                                     
 # Script: Linux Configuration File                                             
 # Description: LCF is a simple script to personalize your Linux distro as H4M1O.
-# Version: 2.0.2                                                                 
+# Version: 2.2.2                                                                 
 # Date: 27-05-2016                                                               
 # Author: Claudio Proietti                                                       
 # License: The MIT License (MIT) - Copyright (c) 2016 Claudio Proietti
 
-function main ()                                                                                                                                 {                                                                                    
+function main ()
+{                                                                                    
+    # control the OS to determine if is Ubuntu or Debian
+    uname -a | egrep Ubuntu
+    if [ $? -eq 0 ] ; then
+        SO=0
+        echo "Ubuntu installed!!!"
+    else
+        SO=1
+        echo "Debian installed!!!"
+    fi
+
+    # control if sudo is installed and usable
+    sudo echo "Testing sudo!"
+    if [ $? -eq 0 ] ; then
+        SU="sudo"
+        echo "Sudo installed!!!"
+    else
+        SU=""
+        echo "Sudo not installed!!!"
+    fi
+
     # This is the main function that show the menu and allows the user to make a choice
     # declared integer for user's choice                                             
-    sudo apt-get update 
-    sudo apt-get upgrade -y
+    echo "$($SU apt-get update)" 
+    echo "$($SU apt-get upgrade -y)"
+    
     clear                                                                            
     declare -i OPT                                                                   
     while true                                                                       
@@ -18,30 +40,46 @@ function main ()                                                                
         menu                                                                         
         # read input from keyboard                                                   
         read OPT                                                                     
-        if [ $OPT -ge 0 -a $OPT -le 6 ]                                              
+        if [ $OPT -ge 0 -a $OPT -le 7 ]                                              
         then                                                                         
             case $OPT in                                                             
-                1 ) bash_cfg                                                     
+                1 ) 
+                bash_cfg                                                     
                 echo -e "$(tput setaf 0)$(tput setab 2)\nBASH CONFIGURATION COMPLETED!$(tput sgr 0)\n"
                 ;;                                                                   
-                2 ) vim_cfg
+                2 )
+                gac_cfg $SU
+                echo -e "$(tput setaf 0)$(tput setab 2)\nGIT AND CURL INSTALLATION AND CONFIGURATION COMPLETED!$(tput sgr 0)\n"                
+                ;;
+                3 )
+                vim_cfg $SU
                 echo -e "$(tput setaf 0)$(tput setab 2)\nVIM INSTALLATION AND CONFIGURATION COMPLETED!$(tput sgr 0)\n"                
                 ;;
-                3 ) tmux_cfg                                                     
+                4 )
+                tmux_cfg $SU                                                    
                 echo -e "$(tput setaf 0)$(tput setab 2)\nTMUX INSTALLATION AND CONFIGURATION COMPLETED!$(tput sgr 0)\n"
                 ;;                                                                   
-                4 ) i3_cfg
+                5 )
+                i3_cfg $SU
                 echo -e "$(tput setaf 0)$(tput setab 2)\nI3 INSTALLATION AND CONFIGURATION COMPLETED!$(tput sgr 0)\n"                
                 ;;
-                5 ) sec_cfg                                                     
+                6 )
+                sec_cfg $SU                                                    
                 echo -e "$(tput setaf 0)$(tput setab 2)\nSECURITY INSTALLATION AND CONFIGURATION COMPLETED!$(tput sgr 0)\n"
                 ;;                                                                   
-                6 ) all_cfg
+                7 ) 
+                root_cfg
+                echo -e "$(tput setaf 0)$(tput setab 2)\nAPPS INSTALLATION AND CONFIGURATION COMPLETED!$(tput sgr 0)\n"
+                exit 1                
+                ;;
+                8 ) echo "$($SU user_inst)"
+                user_cfg $SU
                 echo -e "$(tput setaf 0)$(tput setab 2)\nAPPS INSTALLATION AND CONFIGURATION COMPLETED!$(tput sgr 0)\n"
                 exit 1                
                 ;;
                 0 ) clear
                 echo -e "$(tput setaf 7)$(tput setab 0)\nThank you for using this script!$(tput sgr 0)\n"
+                exit 1
                 ;;                                                                   
             esac                                                                     
         else                                                                         
@@ -54,18 +92,21 @@ function main ()                                                                
 
 function menu ()
 { 
-# The scope of this function is only to show the contextual menu                 
-echo "$(tput setaf 3)$(tput bold)Welcome to L.C.F. - Linux Configuration Files"
-echo -e "Script created by Claudio Proietti under MIT license$(tput sgr 0)\n"
-echo -e "These are the available options:\n"                                 
-echo "1 - CONFIGURE BASH"       
-echo "2 - INSTALL AND CONFIGURE VIM" 
-echo "3 - INSTALL AND CONFIGURE TMUX"       
-echo "4 - INSTALL AND CONFIGURE I3" 
-echo "5 - INSTALL AND CONFIGURE SSH" 
-echo "6 - INSTALL AND CONFIGURE ALL THE APPS" 
-echo -e "\n0 - Exit the script\n"                                            
-echo "$(tput setaf 3)Write now the option that you want select and press enter: $(tput sgr 0)"  
+    # The scope of this function is only to show the contextual menu                 
+    echo "$(tput setaf 3)$(tput bold)Welcome to L.C.F. - Linux Configuration Files"
+    echo -e "Script created by Claudio Proietti under MIT license$(tput sgr 0)\n"
+    echo -e "These are the available options:\n"                                 
+    echo "1 - CONFIGURE BASH"       
+    echo "2 - INSTALL AND CONFIGURE VIM" 
+    echo "3 - INSTALL AND CONFIGURE GIT AND CURL" 
+    echo "4 - INSTALL AND CONFIGURE TMUX"       
+    echo "5 - INSTALL AND CONFIGURE I3 (Requires sudo to work!)"
+    echo "6 - INSTALL AND CONFIGURE SSH" 
+    echo "7 - INSTALL AND CONFIGURE ALL THE APPS FOR ROOT (NO SUDO!!!)"
+    echo "8 - INSTALL AND CONFIGURE ALL THE APPS FOR A STANDARD USER (Require 
+    root or sudo)" 
+    echo -e "\n0 - Exit the script\n"                                            
+    echo "$(tput setaf 3)Write now the option that you want select and press enter: $(tput sgr 0)"  
 }      
 
 function bash_cfg ()
@@ -74,31 +115,37 @@ function bash_cfg ()
     source ~/.bashrc
 }
 
+function gac_cfg ()
+{
+    echo "$($1 apt-get install curl -y)"
+    echo "$($1 apt-get install git -y)"
+}
+
 function vim_cfg ()
 {
-    sudo apt-get install vim
+    echo "$($1 apt-get install vim -y)"
     cp -r .vim ~/
     cp .vimrc ~/
     cp .plugins_vim ~/
-    neb_cfg
+    neb_cfg $1
 }
 
 function tmux_cfg ()
 {
-    sudo apt-get install tmux
+    echo "$($1 apt-get install tmux -y)"
     cp .tmux.conf ~/
 }
 
 function i3_cfg ()
 {
-    sudo apt-get install i3
+    echo "$($1 apt-get install i3 -y)"
     cp -r .i3 ~/
-    sudo cp etc/i3status.conf /etc/i3status.conf
+    echo "$($1 cp etc/i3status.conf /etc/i3status.conf)"
 }
 
 function sec_cfg ()
 {
-    sudo apt-get install openssl-client
+    echo "$($1 apt-get install openssl-client -y)"
     ssh-keygen -b 4096 -t rsa
 }
 
@@ -114,16 +161,6 @@ function neb_cfg ()
     echo "$INSTALL_DIR"
     if [ -e "$INSTALL_DIR" ]; then
         echo "$INSTALL_DIR already exists!"
-    fi
-
-    # install git
-    sudo apt-get install git -y
-    # check git command
-    if type git; then
-        echo 'Git already present!'
-    else
-        echo 'Please install git or update your path to include the git executable!'
-        exit 1
     fi
 
     # make bundle dir and fetch neobundle
@@ -174,13 +211,22 @@ function neb_cfg ()
     echo "colorscheme molokai" >> ~/.plugins_vim
 }
 
-function all_cfg ()
+function root_cfg ()
 {
     bash_cfg
     vim_cfg
     tmux_cfg
     i3_cfg
     sec_cfg
+}
+
+function user_cfg ()
+{
+    bash_cfg $1
+    vim_cfg $1
+    tmux_cfg $1
+    i3_cfg $1
+    sec_cfg $1
 }
 
 #Call to the main function
